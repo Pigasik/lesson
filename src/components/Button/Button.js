b65f1bc7100617bd722c8ba07d8d8139b933904c
VeronikaPigasova <vpigasova96@gmail.com>
2022-11-10 10:58:11 +0300

    lesson1

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import styles from './Button.module.css';
+
+export const Button = (props) => {
+    const {disabled, title, type, click} = props;
+    const st ={
+        primary: styles.primary,
+        secondary: styles.secondary,
+    };
+    const getFirstLeters = (name) => name.split(' ').map(el => el[0].toUpperCase()).join('');
+    return <button disabled={disabled}className={`${styles.main} ${st[type]}`}  onClick={click}>
+        <div className={styles.letters}>{getFirstLeters(title)}</div>
+        {title}
+        </button>
+};

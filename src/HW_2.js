eb565d59c7e9bdafa5b6be8619890acefa829067
VeronikaPigasova <vpigasova96@gmail.com>
2022-11-21 11:32:16 +0300

    hw2

---
+++ src/HW_2.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Form } from './components/Form/Form';
+
+
+function HW_2() {
+   
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default HW_2;
+
+ 
